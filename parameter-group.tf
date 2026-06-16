9976499c92d627ff52cee5f9d080a7dac080d082
learnawscloud2026-blip <learnawscloud2026@gmail.com>
2026-06-16 18:52:06 +0530

    Added some more parameter groups

--- parameter-group.tf
+++ parameter-group.tf
@@ -1,4 +1,5 @@
 resource "aws_db_parameter_group" "mysql_pg" {
+
   name   = "terraform-mysql-pg"
   family = "mysql8.0"
 
@@ -7,7 +8,32 @@ resource "aws_db_parameter_group" "mysql_pg" {
     value = "200"
   }
 
-  tags = {
-    Name = "terraform-mysql-pg"
+  parameter {
+    name  = "slow_query_log"
+    value = "1"
+  }
+
+  parameter {
+    name  = "long_query_time"
+    value = "2"
+  }
+
+  parameter {
+    name  = "log_queries_not_using_indexes"
+    value = "1"
+  }
+
+  parameter {
+    name  = "log_output"
+    value = "FILE"
+  }
+
+  parameter {
+    name  = "event_scheduler"
+    value = "ON"
+  }
+  parameter {
+    name  = "general_log"
+    value = "1"
   }
 }
